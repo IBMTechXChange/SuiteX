10d5f44b8201898a0c8e9019033cd3ef317ddc3a
slaygun <nabhraghav@gmail.com>
2024-09-11 18:40:45 +0530

    styles folder added seperately

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className='gap-16 p-8 pb-20 sm:p-20'>SuiteX</div>;
+  return (
+  <div className='gap-16 p-8 pb-20 sm:p-20'>
+    SuiteX
+    <Button>hi</Button>
+  </div>
+  )
 }
